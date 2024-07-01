836c339b4675b7d5295fcddf47d6ce82f61da43f
c-Harrod <101145026+c-Harrod@users.noreply.github.com>
2024-07-01 12:22:00 -0400

    Update VideoGameRepository.java
    Changes parent interface to JpaRepository from CrudRepository

--- projzero/src/main/java/net/revature/projzero/repository/VideoGameRepository.java
+++ projzero/src/main/java/net/revature/projzero/repository/VideoGameRepository.java
@@ -2,16 +2,16 @@ package net.revature.projzero.repository;
 
 import jakarta.transaction.Transactional;
 import net.revature.projzero.model.VideoGame;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface VideoGameRepository extends CrudRepository<VideoGame, Integer> {
+public interface VideoGameRepository extends JpaRepository<VideoGame, Integer> {
 
     @Modifying
     @Transactional

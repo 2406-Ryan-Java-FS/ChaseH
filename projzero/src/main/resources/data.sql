8714cd51b6b7754c8d7450235284b6a401aad338
c-Harrod <101145026+c-Harrod@users.noreply.github.com>
2024-07-02 10:12:09 -0400

    Update data.sql
    adds commands to create new accounts table and populate table
    
    adds foreign key referencing accounts table primary key and updates insert statements to match

--- projzero/src/main/resources/data.sql
+++ projzero/src/main/resources/data.sql
@@ -1,12 +1,24 @@
 drop table if exists videogames;
+drop table if exists accounts;
 
-CREATE table  videogames (
-    game_id SERIAL PRIMARY KEY,
-    game_name varchar(255) NOT NULL,
-    rating INTEGER,
-    genre varchar(25)
+
+create table accounts(
+	account_id serial primary key,
+	username varchar(255) not null unique,
+	password varchar(255)
 );
 
 
-insert into videogames (game_name, rating, genre) values ('Destiny 2', 9, 'FPS');
-insert into videogames (game_name, rating, genre) values ('World of Warcraft', 10, 'MMORPG');
+CREATE table videogames (
+    game_id SERIAL PRIMARY KEY,
+    game_name varchar(255),
+    rating INTEGER,
+    genre varchar(25),
+    account_id integer references accounts(account_id)
+);
+
+insert into accounts values (default, 'user1', 'password');
+insert into accounts values (default, 'user2', '1234');
+
+insert into videogames (game_name, rating, genre, account_id) values ('Destiny 2', 9, 'FPS', 1);
+insert into videogames (game_name, rating, genre, account_id) values ('World of Warcraft', 10, 'MMORPG', 2);
